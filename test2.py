3bf1933abfd5776b26b43711874d1530bca75209
Matt Teter <mteter9530@gmail.com>
2016-12-13 10:18:56 -0500

    12/13/16, added test3

--- test2.py
+++ test2.py
@@ -1,15 +1,15 @@
 import pygame
 import os
 from time import sleep
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
  
 #Note #21 changed to #27 for rev2 Pi
 button_map = {23:(255,0,0), 22:(0,255,0), 27:(0,0,255), 18:(0,0,0)}
  
 #Setup the GPIOs as inputs with Pull Ups since the buttons are connected to GND
-GPIO.setmode(GPIO.BCM)
-for k in button_map.keys():
-    GPIO.setup(k, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setmode(GPIO.BCM)
+# for k in button_map.keys():
+#     GPIO.setup(k, GPIO.IN, pull_up_down=GPIO.PUD_UP)
  
 #Colours
 WHITE = (255,255,255)
@@ -25,11 +25,14 @@ font_big = pygame.font.Font(None, 100)
 
 while True:
     # Scan the buttons
-    for (k,v) in button_map.items():
-        if GPIO.input(k) == False:
-            lcd.fill(v)
-            text_surface = font_big.render('%d'%k, True, WHITE)
-            rect = text_surface.get_rect(center=(160,120))
-            lcd.blit(text_surface, rect)
-            pygame.display.update()
+    for ii in range(0, 255):
+        lcd.fill((ii, (ii * 2) % 255, (ii * 4) % 255))
+        pygame.display.update()
+    # for (k,v) in button_map.items():
+        # if GPIO.input(k) == False:
+        #     lcd.fill(v)
+        #     text_surface = font_big.render('%d'%k, True, WHITE)
+        #     rect = text_surface.get_rect(center=(160,120))
+        #     lcd.blit(text_surface, rect)
+        #     pygame.display.update()
     sleep(0.1)
