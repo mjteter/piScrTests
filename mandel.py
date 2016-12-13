e0670ca0c335941b7ef29b9ba16d85d6cbc162c5
Matt Teter <mteter9530@gmail.com>
2016-12-13 15:02:34 -0500

    test

--- mandel.py
+++ mandel.py
@@ -3,8 +3,7 @@ import os
 from time import sleep
 
 
-# Colours
-WHITE = (255, 255, 255)
+
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
@@ -13,11 +12,14 @@ lcd = pygame.display.set_mode((320, 240))
 lcd.fill((0, 0, 0))
 pygame.display.update()
 
+# Colours
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
 
+lcd.set_at((0, 10), WHITE)
 lcd.set_at((10, 10), WHITE)
-lcd.set_at((20, 10), WHITE)
-lcd.set_at((30, 10), WHITE)
-lcd.set_at((40, 100), WHITE)
+lcd.set_at((30, 240), WHITE)
+lcd.set_at((40, 239), BLUE)
 pygame.display.update()
 
 sleep(5)
